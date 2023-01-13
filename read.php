52894e6ad1f78586cc067419171edbb88e82e632
KomtGoedd <117632275+KomtGoedd@users.noreply.github.com>
2023-01-13 11:25:18 +0100

    initial commit

---
+++ read.php
@@ -0,0 +1,88 @@
+<?php
+ /**
+  * Maak een verbinding met de mysql-server en database
+  */
+  require('config.php');
+
+  // Maak een data sourcename string
+  $dsn = "mysql:host=$dbHost;dbname=$dbName;charset=UTF8";
+
+  try {
+    $pdo = new PDO($dsn, $dbUser, $dbPass);
+    if ($pdo) {
+        // echo "De verbinding is gelukt";
+    } else {
+        echo "Interne server-error";
+    }
+  } catch(PDOException $e) {
+    echo $e->getMessage();
+  }
+
+/**
+ * Maak een select query die alle records uit de tabel Persoon haalt
+ */
+  $sql = "SELECT Id
+                ,Merk
+                ,Model
+                ,Topsnelheid
+                ,Prijs
+          FROM RichestPeople
+          ORDER BY Topsnelheid DESC";
+
+  // Maak de sql-query gereed om te worden uitgevoerd op de database
+  $statement = $pdo->prepare($sql);
+
+  // Voer de sql-query uit op de database
+  $statement->execute();
+
+  // Zet het resultaat in een array met daarin de objecten (records uit de tabel Persoon)
+  $result = $statement->fetchAll(PDO::FETCH_OBJ);
+
+  // Even checken wat we terugkrijgen
+  // var_dump($result);
+
+  $rows = "";
+  foreach ($result as $info) {
+    $rows .= "<tr>
+                <td>$info->Name</td>
+                <td>$info->Networth</td>
+                <td>$info->Age</td>
+                <td>$info->MyCompagny</td>
+                <td>
+                    <a href='delete.php?Id=$info->Id'>
+                        <img src='img/b_drop.png' alt='kruis'>
+                    </a>
+                </td>
+              </tr>";
+  }
+  
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RichestPeople</title>
+</head>
+<body>
+    <h3>De vijf rijkste mensen ter wereld</h3>
+    <table border='1'>
+        <thead>
+            <th>Naam</th>
+            <th>Vermogen</th>
+            <th>Leeftijd</th>
+            <th>Bedrijf</th>
+            <th>Delete</th>
+        </thead>
+        <tbody>
+            <?= $rows; ?>
+        </tbody>
+    </table>
+</body>
+</html>
+
